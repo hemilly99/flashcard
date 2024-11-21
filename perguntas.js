3ec2c81affb471a28eb75609ac81561604c19bc4
hemilly99 <00001100041539sp@al.educacao.sp.gov.br>
2024-11-21 09:33:18 -0300

    salvando

--- perguntas.js
+++ perguntas.js
@@ -17,7 +17,7 @@ criaCartao(
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Curioso',
+    'Como deixar alguém curioso?',
+    'Eu conto amanhã'
 )
